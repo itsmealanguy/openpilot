6c4376b41287ac95f1b106a583e25b0e1dd90f62
wirelessnet2 <37725998+wirelessnet2@users.noreply.github.com>
2020-07-26 14:44:04 -0400

    can_rcv_error

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -195,7 +195,7 @@ class Controls:
                                         LaneChangeState.laneChangeFinishing]:
       self.events.add(EventName.laneChange)
 
-    if self.can_rcv_error or (not CS.canValid and self.sm.frame > 5 / DT_CTRL):
+    if (not CS.canValid and self.sm.frame > 5 / DT_CTRL):
       self.events.add(EventName.canError)
     if self.mismatch_counter >= 200:
       self.events.add(EventName.controlsMismatch)
